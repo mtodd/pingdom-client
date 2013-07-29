8d4db5556ead8b3b55656f9e32e76b0edef34964
Dhananjay Sathe <dhananjaysathe@gmail.com>
2013-07-29 19:23:55 +0530

    Fix documentation

---
+++ lib/pingdom/analysis.rb
@@ -0,0 +1,15 @@
+module Pingdom
+  
+  # {"id"=>259103, "timefirsttest"=>203173, "timeconfirmtest"=>1298102416}
+  class Analysis < Base
+    def self.parse(client, response)
+      analysis = super
+      Array.wrap(analysis[:analysis]).map do |analysis_element|
+        new(client, response, analysis_element)
+      end
+    end
+    
+    attributes  :timefirsttest    => :time_start,
+                :timeconfirmtest  => :time_confirm,
+  end  
+end
